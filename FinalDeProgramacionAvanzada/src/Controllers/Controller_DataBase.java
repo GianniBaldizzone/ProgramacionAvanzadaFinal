30f84d465ca06af19aec19ff4a750fdcd570dcbe
Gianni Baldizzone <gianni.baldizzone@davinci.edu.ar>
2024-01-20 18:30:52 -0300

    Controlller_Usuario Base

--- FinalDeProgramacionAvanzada/src/Controllers/Controller_DataBase.java
+++ FinalDeProgramacionAvanzada/src/Controllers/Controller_DataBase.java
@@ -1,10 +1,31 @@
 package Controllers;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import javax.swing.JOptionPane;
 public class Controller_DataBase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	
 
+
+Connection con ;
+			
+			public Connection conectar() {
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					
+					
+					con = DriverManager.getConnection("jdbc:mysql://localhost:3306/alohanet","root","jaimito");
+					//JOptionPane.showMessageDialog(null, "se conecto");
+				} catch (Exception e) {
+			
+					JOptionPane.showMessageDialog(null, "error al conectarse");
+				}
+				return con;
+			}
+		
 	}
 
-}
+
